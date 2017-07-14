cd110b40c2ab1cd2e5f295e3d5e7602c63621afa
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-07-13 18:49:12 -0700

    async and error handling

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 
 const storeController = require('../controllers/storeController');
-
+const { catchError } = require('../handlers/errorHandlers');
+ 
 // Do work here
 router.get('/', storeController.homePage);
+
+//
 router.get('/add', storeController.addStore);
+router.post('/add', catchError(storeController.createStore));
 
 module.exports = router;
