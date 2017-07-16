8c7651a6c8633c9c0c75516318aab78c617e0011
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-07-16 16:00:48 -0700

    search

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -3,5 +3,9 @@ import '../sass/style.scss';
 import { $, $$ } from './modules/bling';
 import autoComplete from './modules/autocomplete';
 
+import typeAhead from './modules/typeAhead.js'
+
 // id was defined in _storeForm.pug
 autoComplete($('#address'), $('#lat'), $('#lng'));
+
+typeAhead($('.search'));
