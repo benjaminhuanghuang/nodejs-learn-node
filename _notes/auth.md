17177fef8f334adcd6d89c20b3dd898faa49767d
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-07-16 10:11:10 -0700

    add permission checking to router

--- _notes/auth.md
+++ _notes/auth.md
@@ -6,3 +6,16 @@
     layout.pug
         li.nav__item: a.nav__link(href="/account", class=(currentPath.startsWith('/account') ? 'nav__link--active' : ''))
                 img.avatar(src=user.gravatar + 'd=retro')
+
+    in user.js
+        add virtual field "gravatar" for user
+
+
+## Permission checking
+    in authController.js
+        exports.isLoggedIn = (req, res, next) => {
+
+        });
+
+    in index.js, add middleware to the router
+        router.get('/add', authController.isLoggedIn, storeController.addStore);
--- controllers/authController.js
+++ controllers/authController.js
@@ -7,9 +7,20 @@ exports.login = passport.authenticate('local', {
     successFlash: 'You are now logged in!'
 });
 
-exports.logout = (req, res) =>{
+exports.logout = (req, res) => {
     req.logout();
     req.flash('success', 'You are now logged out!');
     res.redirect('/');
 }
 
+// Middleware for permission checking
+exports.isLoggedIn = (req, res, next) => {
+    // Using the function in passport middle ware
+    if (req.isAuthenticated()) {
+        next();
+        return;
+    }
+    //
+    req.flash('error', 'You must be logged in to do that');
+    res.redirect('/login');
+}
